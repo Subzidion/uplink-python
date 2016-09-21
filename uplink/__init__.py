1e9bfef8743be70dea9de00f871ab697337fa6ee
Carl 'Subzidion' Hiltbrunner <carlhiltbrunner@hotmail.com>
2016-09-21 17:19:50 -0400

    Add secret key to app initialization and add secrets.py file

--- uplink/__init__.py
+++ uplink/__init__.py
@@ -3,18 +3,20 @@ import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from .secrets import SECRET_KEY
 
-from . import models
+db = SQLAlchemy()
 
 def create_app(testing=False):
     app = Flask(__name__)
+    app.secret_key = SECRET_KEY 
     basedir = os.path.abspath(os.path.dirname(__file__))
-    if testing == True:
+    if testing:
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'db.sqlite'))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['DEBUG'] = True
 
     db.init_app(app)
     
